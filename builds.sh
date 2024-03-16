0cc8519934da4566561c33c046dd6f25b627fe21
Alec Vince <aleclightning14@gmail.com>
2024-03-17 02:51:58 +0800

    fixed the webapp.py to only included whats needed

--- builds.sh
+++
@@ -1,8 +0,0 @@
-# Update package lists
-sudo apt-get update
-
-# Install required packages
-sudo apt-get install vsftpd 
-
-sudo systemctl start vsftpd
-sudo systemctl enable vsftpd
--- webapp.py
+++ webapp.py
@@ -1,6 +1,4 @@
-from flask import Flask, jsonify, render_template, send_file,request
-from ftplib import FTP
-import os
+from flask import Flask, jsonify, render_template, send_file
 
 # NOTE: Create a new Flask app
 app = Flask(__name__)
@@ -27,64 +25,6 @@ def download_file():
     # NOTE: Using the native Flask send_file function to send the file
     return send_file(file_path, mimetype='text/plain', as_attachment=True, download_name='download_simple.txt')
 
-
-# NOTE: The donwloaded route returns a file to be downloaded
-@app.route('/ftp/download', methods=['POST'])
-def ftp_download():
-
-    #NOTE: Here are using the FTPlib to connect to the FTP server and download the file
-
-    ftp_host = 'ftp.dlptest.com'
-    ftp_user = 'dlpuser'
-    ftp_password = 'rNrKYTX9g7z3RgJRmxWuGHbeu'
-    ftp_directory = ''
-    
-    #NOTE: using the request.form.get to get the filename from the form data
-    file_name = request.form.get('filename')
-
-    # NOTE: Here are using the FTPlib to connect to the FTP server with credentials above
-
-    # Create an FTP object and connect to the server
-    ftp = FTP(ftp_host)
-
-    # Login to the server with user and password
-    ftp.login(user=ftp_user, passwd=ftp_password)
-
-    # Change the directory to the FTP directory
-    ftp.cwd(ftp_directory)
-
-    
-    local_file_path = os.path.join('', file_name)
-    with open(local_file_path, 'wb') as local_file:
-        ftp.retrbinary('RETR ' + file_name, local_file.write)
-
-    ftp.quit()
-    return f'File "{file_name}" downloaded to "{local_file_path}"'
-
-
-# NOTE: The upload route returns a file to be uploaded
-@app.route('/ftp/upload', methods=['POST'])
-def ftp_upload():
-
-    #NOTE: Here are using the FTPlib to connect to the FTP server and download the file
-
-    ftp_host = 'ftp.dlptest.com'
-    ftp_user = 'dlpuser'
-    ftp_password = 'rNrKYTX9g7z3RgJRmxWuGHbeu'
-    ftp_directory = ''
-
-    file = request.files['file']
-    file_name = file.filename
-
-    ftp = FTP(ftp_host)
-    ftp.login(user=ftp_user, passwd=ftp_password)
-    ftp.cwd(ftp_directory)
-
-    with open(file_name, 'rb') as local_file:
-        ftp.storbinary('STOR ' + file_name, local_file)
-
-    ftp.quit()
-    return f'File "{file_name}" uploaded to FTP server'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
